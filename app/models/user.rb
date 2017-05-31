e08ef518c192a76ccb6f5349cb42f5676f726961
folkens2 <tfolkens@chicagobooth.edu>
2017-05-31 09:51:49 -0500

    add user data table

---
+++ app/models/user.rb
@@ -0,0 +1,9 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+  :recoverable, :rememberable, :trackable, :validatable
+
+  has_many :reviews, :foreign_key => "reviewer_id", :dependent => :destroy
+
+end
