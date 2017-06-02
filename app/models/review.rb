069ec19bfc09cd05f14a3466cea6a535a65f80f3
folkens2 <tfolkens@chicagobooth.edu>
2017-06-02 12:22:31 -0500

    validations for rating to be an integer between 1-5

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,7 +1,10 @@
 class Review < ApplicationRecord
-  
-  belongs_to :user, :foreign_key => "reviewer_id"
 
+  # Associations
+  belongs_to :user, :foreign_key => "reviewer_id"
   belongs_to :tutor
+
+  # Validations
+  validates :rating, :numericality => { :only_integer => true, :allow_blank => false, :greater_than_or_equal_to => 1, :less_than_or_equal_to => 5}
 
 end
