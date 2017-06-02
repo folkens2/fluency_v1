3cb1cf2e0829f1b109fb2e3a7d2621ac421cf08f
folkens2 <tfolkens@chicagobooth.edu>
2017-06-02 11:04:57 -0500

    tried has_many / through relationship; average rating attempt

--- app/models/tutor.rb
+++ app/models/tutor.rb
@@ -2,6 +2,8 @@ class Tutor < ApplicationRecord
 
   has_many :reviews, :dependent => :destroy
 
+  # has_many :ratings, :through => :reviews
+
   belongs_to :instruction_lang_skill, :class_name => "Skill"
 
   belongs_to :instruction_lang, :class_name => "Language"
