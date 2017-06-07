2ff48e50ced6f2755de55319a7be6844244b0253
folkens2 <tfolkens@chicagobooth.edu>
2017-06-07 15:06:09 -0500

    fixed tutor reviews page where link takes you to the reviews only for that tutor

--- app/controllers/tutorreviews_controller.rb
+++ app/controllers/tutorreviews_controller.rb
@@ -1,7 +1,8 @@
 class TutorreviewsController < ApplicationController
   def show
     @reviews = Review.all
-    @tutor_reviews = @reviews.where(:tutor_id => :id)
+    @tutor = Tutor.find(params[:id])
+    @tutor_reviews = Review.where(:tutor_id => @tutor.id)
 
     render("tutor_reviews/show.html.erb")
   end
